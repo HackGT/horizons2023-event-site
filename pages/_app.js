405556fdc8db3269b92b81e12082146742b6a9a8
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-02-15 01:42:12 -0500

    switch from parcel to next.js

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.scss'
+import '../styles/styles.scss';
+import { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,222 @@
+import Image from 'next/image';
+
+function MainPage() {
+    return (
+        <body>
+            <section id="section-hero">
+                <div className="hero-container">
+                <h1 className="hero-title">Horizons</h1>
+                <p>Georgia Tech</p>
+                <p>Instructional Center, Mason Building</p>
+                <p>March 12th-13th</p>
+                </div>
+                <img
+                    src="/img/juice-mushroom.png"
+                    alt="illustration of a mushroom"
+                    className="hero-img"
+                />
+            </section>
+            <section id="section-about">
+                <div className="about-container">
+                <h1>About</h1>
+                <p>There's a new event on the Horizon 🌅 👀!</p>
+                <p>
+                    HexLabs is thrilled to welcome you to 🎨
+                    <strong>Horizons: a 24-hour arts + tech make-a-thon!</strong> This event
+                    is open to anyone who wants to create for the sake of creating-
+                    regardless of major, skillset, or experience. Whether you're an art
+                    aficionado, a low-key music mixer 🎶, code wizard, crocheting
+                    connoisseur, an engineer at heart 🎠, or a passionate storyteller 🎬,
+                    your intrinsic connection to creativity is what makes Horizons THE event
+                    for you!
+                </p>
+                <p>
+                    🌿 From March 12-13th, explore your unique skills and ideas at Georgia
+                    Tech 🌱 ✨.
+                </p>
+                <p>
+                    During this weekend, participants will brainstorm, design, and create
+                    projects of their interests that integrate arts and technology while
+                    attending fun workshops and mini-events. At the end of the event, there
+                    will also be a display of all the projects where judges will vote for
+                    specific categories and award prizes to winning teams. 👀
+                </p>
+                </div>
+                <img
+                src="/img/basket-mushrooms.png"
+                alt="falling mushrooms and a basket"
+                className="about-img"
+                />
+            </section>
+            <section id="section-registration">
+                <img
+                src="/img/flowers-0.png"
+                alt="a blue and pink flower"
+                className="registration-img"
+                />
+                <div className="registration-container">
+                <h1>Registration</h1>
+                <p>Registration is FREE and is open NOW. It closes February 28th.</p>
+                <a href="https://forms.gle/RCz4cEZgmgqmHvocA">Register Now!</a>
+                </div>
+            </section>
+            <section id="section-schedule">
+                <div className="schedule-container">
+                <h1>Schedule</h1>
+                <p>Schedule Coming Soon!</p>
+                <p>
+                    *Note: The schedule is subject to change, and will be updated as more
+                    details arrive :)*
+                </p>
+                <img
+                    src="/img/quote-bg.png"
+                    alt="a bear holding paint"
+                    className="schedule-img-background"
+                />
+                </div>
+                <img
+                src="/img/bear.png"
+                alt="a quote bubble surrounded with flowers"
+                className="schedule-img"
+                />
+            </section>
+            <section id="section-faq">
+                <h1>Frequently Asked Questions</h1>
+
+                <p>
+                <strong>So I've heard of a hackathon, but what's a make-a-thon?</strong>
+                </p>
+                <p>
+                A make-a-thon is a creativity-driven event where teams make their ideas
+                into reality with resources, materials, and support from HexLabs!
+                </p>
+
+                <p><strong>Can I attend?</strong></p>
+                <p>
+                Anyone who is a Georgia Tech student, or is 18 and above at any Georgia
+                university can attend! With beginner-friendly workshops, fun and relaxing
+                mini-events and great vibes, students from any background can come and
+                enjoy!
+                </p>
+
+                <p><strong>How much will this cost?</strong></p>
+                <p>
+                Horizons is completely free! We will provide the food, swag, and resources
+                you need so you can focus on your most awesome creation.
+                </p>
+
+                <p><strong>Where can I register?</strong></p>
+                <p>
+                Registration is open
+                <a href="https://forms.gle/RCz4cEZgmgqmHvocA">here</a> and will close for
+                participants on Monday, February 28 2020 at 11:59pm
+                </p>
+
+                <p><strong>What should I bring?</strong></p>
+                <p>
+                We request that you bring yourself :relaxed:, your laptop, and your
+                student ID! If you are not a Georgia Tech student you will also need to
+                bring a government ID and student ID. Feel free to bring anything else
+                (art supplies, tools, etc...) that you think would be helpful.
+                </p>
+
+                <p><strong>Where is Horizons?</strong></p>
+                <p>
+                We'll be waiting for you at the Instructional Center, aka,
+                <a href="https://goo.gl/maps/rk4Ks2nU5DwuGHxR6"
+                    >759 Ferst Dr. NW, Atlanta, GA 30318.</a>
+                </p>
+
+                <p><strong>Will there be any makerspaces open during the event?</strong></p>
+                <p>
+                We are currently in discussions of partnering with The Hive and the
+                Invention Studio to offer spaces, tools, and mentorship during the
+                event...
+                </p>
+
+                <p><strong>How long is the event?</strong></p>
+                <p>
+                Two days! Day 1 (Saturday March 12) will start at (TBD) and finish at
+                (TBD); Day 2 (Sunday, March 13) will start at (TBD) and finish at (TBD).
+                </p>
+
+                <p><strong>What resources will be there?</strong></p>
+                <p>
+                We'll be providing lots of arts supplies, hardware, and other cool
+                supplies. A full list of available supplies and services is coming soon.
+                Feel free to tell us what you want to see at the event in your
+                application.
+                </p>
+
+                <p><strong>What size can teams be?</strong></p>
+                <p>
+                The team can be up to 4 people in size! Let your friends know to register
+                ASAP.
+                </p>
+
+                <p><strong>My friends are out of town! How do I find a team?</strong></p>
+                <p>
+                We will have a mixer at the start of the event to help you find a team,
+                but if you prefer going solo, we’re here by your side too!
+                </p>
+
+                <p><strong>Any other questions?</strong></p>
+                <p>
+                Email us at <a href="mailto:hello@hexlabs.org">hello@hexlabs.org 💜</a>.
+                </p>
+            </section>
+            <section id="section-express">
+                <div className="express-container">
+                <h1>Express Yourself</h1>
+                <p>
+                    Horizons is a celebration of the incredible intersection between art and
+                    technology. It is a platform for people to express themselves through
+                    art while using technology or engineering to augment its impact.
+                </p>
+                <p>
+                    Moreso, art and technology come together as avenues for people to share
+                    their ideas. To share their findings. To share their feelings. To
+                    express themselves.
+                </p>
+                <p>At Horizons, we challenge you to do just that!</p>
+                </div>
+                <div className="express-container-quote">
+                <img
+                    src="/img/flowers-1.png"
+                    alt="flowers and butterflies"
+                    className="quote-img"
+                />
+                <div>
+                    <p>
+                    “The art challenges the technology, and the technology inspires the
+                    art.”
+                    </p>
+                    <p>- John Lasseter</p>
+                </div>
+                </div>
+            </section>
+            <footer>
+                <p><strong>We can't wait to see you expand your horizons 🌄😉!</strong></p>
+                <p>With 💜 from the HexLabs team</p>
+
+                <div className="icon-container">
+                <a href="https://www.instagram.com/thehexlabs/"
+                    ><img src="/icon/insta.svg"
+                /></a>
+                <a href="https://www.facebook.com/TheHexLabs"
+                    ><img src="/icon/fb.svg"
+                /></a>
+                <a href="https://twitter.com/TheHexLabs"
+                    ><img src="/icon/twitter.svg"
+                /></a>
+                <a href="https://github.com/HackGT"
+                    ><img src="/icon/github.svg"
+                /></a>
+                <a href="https://hexlabs.org/"><img src="/icon/web.svg" /></a>
+                </div>
+            </footer>
+        </body>
+    );
+}
+
+export default MainPage;
