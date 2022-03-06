d452966872bfe9d7bc772b6a67fcf8bf60fe141b
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-03-06 17:32:59 -0500

    add horizons favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import '../styles/globals.scss'
 import '../styles/styles.scss';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+  <Component {...pageProps} />
+    <Head>
+        <link rel="shortcut icon" href="/icon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
+      </Head>
+      
+  </div>
+  );
+  
 }
 
 export default MyApp
